4a6afb0b7b9fe58173b3d589478650151f09ad68
pinar15 <pdasdemir49@gmail.com>
2023-02-17 21:14:20 +0000

    update3

--- main.tf
+++ main.tf
@@ -2,16 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI
   instance_type = var.instance_type
   subnet_id = 	"subnet-0bacae628484819a4"
-  user_data_base64 = base64encode(
-    "#!/bin/bash\n" 
-    "sudo yum update -y\n"
-    "sudo amazon-linux-extras install nginx1.12 -y\n"
-    "sudo systemctl start nginx\n"
-    "sudo systemctl enable nginx\n"
-    "sudo touch /usr/share/nginx/html/index.html\n"
-    "sudo chmod 777 /usr/share/nginx/html/index.html\n"
-    "sudo echo '<html><head><title>Hello World</title></head><body><h1>Hello World!</h1></body></html>' > /usr/share/nginx/html/index.html\n"
-  )
+  user_data_base64 = "IyEvYmluL2Jhc2gKeXVtIHVwZGF0ZSAteQphbWF6b24tbGludXgtZXh0cmFzIGluc3RhbGwgbmdpbngxLjEyIC15CnN5c3RlbWN0bCBzdGFydCBuZ2lueApzeXN0ZW1jdGwgZW5hYmxlIG5naW54CnRvdWNoIC91c3Ivc2hhcmUvbmdpbngvaHRtbC9pbmRleC5odG1sCmNobW9kIDc3NyAvdXNyL3NoYXJlL25naW54L2h0bWwvaW5kZXguaHRtbAplY2hvICc8aHRtbD48aGVhZD48dGl0bGU+SGVsbG8gV29ybGQ8L3RpdGxlPjwvaGVhZD48Ym9keT48aDE+SGVsbG8gV29ybGQhPC9oMT48L2JvZHk+PC9odG1sPicgPiAvdXNyL3NoYXJlL25naW54L2h0bWwvaW5kZXguaHRtbAo="
+
   key_name = "web_key_pair"
 
   tags = {
